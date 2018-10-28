a7ac288666a40b85bfd4c59f9d3de9de283bd408
Silvia Patono <silvia.patono@gmail.com>
2018-10-28 16:58:01 +0100

    first commit

---
+++ phone.php
@@ -0,0 +1,22 @@
+<?php
+
+class Phone
+{
+	public function sendMessage()
+	{
+
+	}
+
+	public function receiveMessage()
+	{
+
+	}
+
+	public function displayMessageAndReceiverNumber()
+	{
+
+	}
+
+}
+
+$phone = new Phone();
