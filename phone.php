bfdf875125a2dded4dc182c2bb530b9fc3d59f89
Silvia Patono <silvia.patono@gmail.com>
2018-11-04 17:54:19 +0100

    Created functions and arrays to store messages

--- phone.php
+++ phone.php
@@ -2,33 +2,54 @@
 
 class Phone
 {
+
+	private $sentMessages = [];
+	private $receivedMessages = [];
+
 	public function sendMessage($to, $message) 
 	{
-
+		$this->sentMessages = [
+			"to" => $to,
+			"message" => $message
+		];
 	}
 
 	public function receiveMessage($from, $message) 
 	{
-
+		$this->receivedMessages = [
+			"from" => $from,
+			"message" => $message
+		];
 	}
 
 	public function seeAllSendedMessage() 
 	{
-
+		return $this->sentMessages;
 	}
 
 
-	seeAllReceivedMessage()
+	public function seeAllReceivedMessage()
 	{
-
+		return $this->receivedMessages;
 	}
 }
 
+$messages = array(
+	array ("Quentino", "Silvia", "Salut Silvia"),
+	array ("Silvia", "Quentino", "Salut Quentino"),
+	array ("Raph", "Silvia", "J'arrive")
+);	
 
 $phone = new Phone();
 
-$phone->sendMessage($to, $message);
-$phone->receiveMessage($from, $message);
-$phone->seeAllSendedMessage();
-$phone->seeAllReceivedMessage();
+foreach ($messages as $message) {
+	$phone->sendMessage($message[1], $message[2]);
+	$phone->receiveMessage($message[0], $message[2]);
+	$sentMessages [] = [$phone->seeAllSendedMessage()];
+	$receivedMessages [] = [$phone->seeAllReceivedMessage()];
+}
+
+var_dump($sentMessages);
+var_dump($receivedMessages);
+
 
